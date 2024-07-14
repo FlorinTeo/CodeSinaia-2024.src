0c27952bc2fcc8f37fa84ffad6cd6f7fb9392dcf
Florin.Teo <Florin.Teo@live.com>
2024-07-14 10:25:41 -0700

    Improve demo user code

--- GraphVisualizer/js/userCode.js
+++ GraphVisualizer/js/userCode.js
@@ -4,17 +4,38 @@ import { graph } from "./main.js";
 
 export class UserCode extends CoreCode {
 
-    async run() {
-        console.out("Start custom code!")
-        await this.step();
-        for(let i = 1; i <= 4; i++) {
-            console.out(`step ${i}`);
-            await this.step();
-        }
-        console.out('Done!');
+    /**
+     * Toggles the colors of each node, if any
+     */
+    async toggleNodes() {
         for(const node of graph.nodes) {
-            node.toggleColor(1);
             await this.step();
+            node.toggleColor(1);
         }
+    }
+
+    /**
+     * Toggles the colors of each edge, if any
+     */
+    async toggleEdges() {
+        for(let i=0; i < graph.edges.length; i++) {
+            await this.step();
+            graph.edges[i].toggleColor(1);
+        }
+    }
+
+    /**
+     * Entry point for user-defined code.
+     */
+    async run() {
+        console.out("Starting to toggle node colors!");
+        await this.toggleNodes();
+        console.out("Done!");
+
+        await this.step();
+        console.out("Starting to toggle edge colors!");
+        
+        await this.toggleEdges();
+        console.out("Done and finish!");
     }
 }
