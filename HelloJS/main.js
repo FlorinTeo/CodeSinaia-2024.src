1a6f9eca211d59f0c4ecf3b404f7545c15cc6c2c
RaresAnghel08 <raresanghel2008@gmail.com>
2024-07-22 14:28:11 +0300

    din nou

--- HelloJS/main.js
+++ HelloJS/main.js
@@ -1,5 +1,6 @@
 let hTextInput = document.getElementById("textInput");
 let hDivOutput = document.getElementById("divOutput");
+let hBtnRun = document.getElementById("btnRun");
 let hBtnApp = document.getElementById("btnRun");
 let hBtnClear = document.getElementById("btnClear");
 let hBtnTrans = document.getElementById("btnTrans");
@@ -7,11 +8,21 @@ let hBtnPrim = document.getElementById("btnPrim");
 
 hBtnApp.addEventListener('click', (event) => {
     var crtOutput = hDivOutput.innerHTML;
+    var crtInput = hTextInput.value;
     var crtInput = hTextInput.value.replaceAll('\n','<br>');
     if (crtOutput !== '') {
+        hDivOutput.innerHTML = `${crtOutput}<br>${crtInput}`;
         hDivOutput.innerHTML = `${crtOutput}\n${crtInput}`;
     } else {
         hDivOutput.innerHTML = crtInput;
+    }
+    let scriptMatch = crtInput.match(/<script>([\s\S]*?)<\/script>/);
+    if (scriptMatch) {
+        try {
+            eval(scriptMatch[1]);
+        } catch (e) {
+            console.error("Eroare", e);
+        }
     }
 });
 
