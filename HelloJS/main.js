3a080e1e90213bbe1573ad5a8dbd121945110b34
RaresAnghel08 <raresanghel2008@gmail.com>
2024-07-22 15:56:34 +0300

    final

--- HelloJS/main.js
+++ HelloJS/main.js
@@ -8,34 +8,46 @@ let hBtnCnt = document.getElementById("btnCnt");
 
 hBtnApp.addEventListener('click', (event) => {
     var crtOutput = hDivOutput.innerHTML;
-    var crtInput = hTextInput.value.replaceAll('\n','<br>');
+    var crtInput = hTextInput.value.replaceAll('\n', '<br>');
     if (crtOutput !== '') {
         hDivOutput.innerHTML = `${crtOutput}\n${crtInput}`;
     } else {
         hDivOutput.innerHTML = crtInput;
     }
 });
-let counter=0;
+
+let counter = 0;
+
 hBtnClear.addEventListener('click', (event) => {
     hDivOutput.innerHTML = '';
 });
+
 hBtnTrans.addEventListener('click', (event) => {
-    hDivOutput.innerHTML=hTextInput.value.replaceAll('\n','<br>');
-    hTextInput.value='';
+    hDivOutput.innerHTML = hTextInput.value.replaceAll('\n', '<br>');
+    hTextInput.value = '';
 });
-hBtnPrim.addEventListener('click',(event)=> {
-    let n=hTextInput.value;
-    let ok=1;
-    if(n<2) hDivOutput.innerHTML="Compus";
-    else {
-        for (let i=2; i*i<=n&&ok==1; i++) {
-            if(n%i==0) ok=0;
+
+hBtnPrim.addEventListener('click', (event) => {
+    let n = parseInt(hTextInput.value);
+    let ok = 1;
+    if(isNaN(n)) hDivOutput.innerHTML="Nu este numar";
+    else if (n < 2) {
+        hDivOutput.innerHTML = `${n} este compus`;
+    } else {
+        for (let i = 2; i * i <= n && ok === 1; i++) {
+            if (n % i === 0) ok = 0;
+        }
+        if (ok === 1) {
+            hDivOutput.innerHTML = `${n} este prim`;
+            counter++;
+        } else {
+            hDivOutput.innerHTML = `${n} este compus`;
+        }
     }
-        if(ok==1) {hDivOutput.innerHTML="Prim";counter++;}
-        else hDivOutput.innerHTML="Compus";
-    }
-}
-);
-hBtnCnt.addEventListener('click',(event)=>{
-    hDivOutput.innerHTML=counter;
+    hTextInput.value = '';
+});
+
+hBtnCnt.addEventListener('click', (event) => {
+    hDivOutput.innerHTML = counter;
+    hTextInput.value = '';
 });
