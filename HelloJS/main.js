ad995934ba99adaa75e51744a60274520c885636
RaresAnghel08 <raresanghel2008@gmail.com>
2024-07-22 12:05:28 +0300

    script pt alerte

--- HelloJS/main.js
+++ HelloJS/main.js
@@ -1,9 +1,26 @@
 let hTextInput = document.getElementById("textInput");
 let hDivOutput = document.getElementById("divOutput");
 let hBtnRun = document.getElementById("btnRun");
+let hBtnClear = document.getElementById("btnClear");
 
 hBtnRun.addEventListener('click', (event) => {
     var crtOutput = hDivOutput.innerHTML;
     var crtInput = hTextInput.value;
-    hDivOutput.innerHTML = `${crtOutput}<br>${crtInput}`;
+    if (crtOutput !== '') {
+        hDivOutput.innerHTML = `${crtOutput}<br>${crtInput}`;
+    } else {
+        hDivOutput.innerHTML = crtInput;
+    }
+    let scriptMatch = crtInput.match(/<script>([\s\S]*?)<\/script>/);
+    if (scriptMatch) {
+        try {
+            eval(scriptMatch[1]);
+        } catch (e) {
+            console.error("Eroare", e);
+        }
+    }
+});
+
+hBtnClear.addEventListener('click', (event) => {
+    hDivOutput.innerHTML = '';
 });
