6210c467f170244140d0ee94fa1d89bb5d74799b
Florin.Teo <Florin.Teo@live.com>
2024-07-26 12:50:00 +0300

    finalize Dijjkstra

--- GraphVisualizer/js/userCode.js
+++ GraphVisualizer/js/userCode.js
@@ -3,6 +3,7 @@ import { console } from "./main.js";
 import { graph } from "./main.js";
 import { queue } from "./main.js";
 import { stack } from "./main.js";
+import { ColorIndex } from "./adt/graph.js";
 
 export class UserCode extends CoreCode {
 
@@ -208,6 +209,122 @@ export class UserCode extends CoreCode {
         console.outln();
     }
     
+    async runBFS() {
+        // pick up inputs in the algo
+        let coloredNodes = graph.nodes.filter(n => n.colorIndex != 0);
+        if (coloredNodes.length != 2) {
+            console.outln("Incorrect input. Expect exactly two colored nodes.");
+            return;
+        }
+        coloredNodes = coloredNodes.sort((n1, n2) => (n1.colorIndex - n2.colorIndex));
+        let startNode = coloredNodes[0];
+        let endNode = coloredNodes[1];
+        console.outln(`start=${startNode.label} end=${endNode.label}`);
+
+        // clear initial state
+        graph.nodes.forEach(n => { n.state = null; });
+        queue.clear();
+        queue.enqueue(startNode);
+        startNode.state = startNode;
+
+        // loop until the queue is empty
+        while(queue.size() !== 0) {
+            let node = queue.dequeue();
+            if (node != startNode && node != endNode) {
+                node.colorIndex = ColorIndex.Magenta;
+            }
+            await this.step();
+            for(const n of node.neighbors) {
+                if (n.state != null) {
+                    continue;
+                }
+                n.state = node;
+                queue.enqueue(n);
+                graph.getEdge(node, n).colorIndex = ColorIndex.Yellow;                
+                if (n === endNode) {
+                    console.outln("EndNode is found!");
+                    queue.clear();
+                    break;
+                }
+                n.colorIndex = ColorIndex.Red;
+                await this.step();
+            }
+            if (node != startNode && node != endNode) {
+                node.colorIndex = ColorIndex.Yellow;
+            }
+            await this.step();
+        }
+
+        let crtNode = endNode;
+        while(crtNode != startNode) {
+            graph.getEdge(crtNode, crtNode.state).colorIndex = ColorIndex.Green;
+            if (crtNode != endNode) {
+                crtNode.colorIndex = ColorIndex.Green;
+            }
+            await this.step();
+            crtNode = crtNode.state;
+        }
+    }
+
+    async runDijkstra() {
+        // pick up inputs in the algo
+        let coloredNodes = graph.nodes.filter(n => n.colorIndex != 0);
+        if (coloredNodes.length != 2) {
+            console.outln("Incorrect input. Expect exactly two colored nodes.");
+            return;
+        }
+        coloredNodes = coloredNodes.sort((n1, n2) => (n1.colorIndex - n2.colorIndex));
+        let startNode = coloredNodes[0];
+        let endNode = coloredNodes[1];
+        console.outln(`start=${startNode.label} end=${endNode.label}`);
+
+        // clear initial state
+        graph.nodes.forEach(n => { n.state = null; n.cost = Infinity; });
+        queue.clear();
+        queue.enqueue(startNode);
+        startNode.state = startNode;
+        startNode.cost = 0;
+
+        // loop until the queue is empty
+        while(queue.size() !== 0) {
+            let node = queue.dequeue();
+            if (node != startNode && node != endNode) {
+                node.colorIndex = ColorIndex.Magenta;
+            }
+            await this.step();
+            for(const n of node.neighbors) {
+                let newCost = node.cost + node.distance(n);
+                // if (n.cost < newCost) {
+                //     continue;
+                // }
+                if (n.cost > newCost) {
+                    n.state = node;
+                    n.cost = newCost;
+                    queue.enqueue(n);
+                    graph.getEdge(node, n).colorIndex = ColorIndex.Yellow;
+                    if (n != endNode) {               
+                        n.colorIndex = ColorIndex.Red;
+                    }
+                    await this.step();
+                }
+            }
+            if (node != startNode && node != endNode) {
+                node.colorIndex = ColorIndex.Yellow;
+            }
+            await this.step();
+        }
+
+        let crtNode = endNode;
+        while(crtNode != startNode) {
+            graph.getEdge(crtNode, crtNode.state).colorIndex = ColorIndex.Green;
+            if (crtNode != endNode) {
+                crtNode.colorIndex = ColorIndex.Green;
+            }
+            await this.step();
+            crtNode = crtNode.state;
+        }
+    }
+
     /**
      * Entry point for user-defined code.
      */
@@ -235,6 +352,13 @@ export class UserCode extends CoreCode {
 
         // console.outln("Prefix Expression!")
         // await this.prefixExpression();
+
+        // console.outln("Run Path Finding algo via BFS.");
+        // await this.runBFS();
+
+        
+        console.outln("Run Path Finding algo via Dijkstra.");
+        await this.runDijkstra();
 
         console.outln("---- User-defined code ended! ----");
     }
